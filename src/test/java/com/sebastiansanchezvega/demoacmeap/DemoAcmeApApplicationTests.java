aa02e4c51c243346ecc00820b7e0c00902244e55
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-19 22:32:39 -0300

    spring files added - initial commit

---
+++ src/test/java/com/sebastiansanchezvega/demoacmeap/DemoAcmeApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiansanchezvega.demoacmeap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAcmeApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
